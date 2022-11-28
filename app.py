bbdbf6cb24a0df0b8220cb8300a30997b3ac3b76
Andy Le <andy.quile@gmail.com>
2022-11-27 16:47:18 -0800

    Add logic to allow app user choose a desired model for classifying tweet

--- app.py
+++ app.py
@@ -4,54 +4,47 @@ import torch
 import transformers
 from transformers import AutoModelForSequenceClassification
 from transformers import AutoTokenizer
-from transformers import BertConfig, BertForSequenceClassification, BertTokenizer
+from transformers import BertConfig, BertTokenizer, BertForSequenceClassification
+from transformers import DistilBertConfig, DistilBertTokenizer, DistilBertForSequenceClassification
 from transformers import pipeline
 
-from flask import Flask,render_template,request
+from flask import Flask, render_template, request
 
 
 app = Flask(__name__)
+
+
 bert_tokenizer = BertTokenizer.from_pretrained("./bert_base_model")
 bert_model = BertForSequenceClassification.from_pretrained("./bert_base_model")
+distilbert_tokenizer = DistilBertTokenizer.from_pretrained("./distilbert_base_model")
+distilbert_model = DistilBertForSequenceClassification.from_pretrained("./distilbert_base_model")
 
 
-def get_pred(sentences):
-	encoded_input = tokenizer([sentences], padding=True, truncation=True, max_length=128, return_tensors='pt')
-
-	with torch.no_grad():
-		labels = torch.tensor([1]).unsqueeze(0)
-		model_output = model(**encoded_input)
-
-		logits = model_output[1]
-		_, preds = torch.max(model_output[1], dim=1)
-		predictions = predictions.extend(preds)
-		predictions = torch.stack(predictions)
-		print("This is the prediction: ", predictions)
-	return predictions
-
-	
-
-@app.route('/')
+@app.route('/', methods=['POST','GET'])
 def home():
-	return render_template('home.html')
+    return render_template('home.html')
 
-@app.route('/predict',methods=['POST'])
+@app.route('/predict', methods=['POST'])
 def predict():
-	message = request.form['message']
-	bert_classifier = pipeline(task="text-classification", 
-								model=bert_model,
-								tokenizer=bert_tokenizer)	
-	output = bert_classifier(message)
-	label = output[0]["label"]
+    message = request.form['message']
+    selected_model = request.form['model']
+    classifier = None
+    
+    if selected_model == "BERT":
+        classifier = pipeline(task="text-classification", 
+                            model=bert_model,
+                            tokenizer=bert_tokenizer)
+    elif selected_model == "DISTILBERT":
+        classifier = pipeline(task="text-classification",
+                            model=distilbert_model,
+                            tokenizer=distilbert_tokenizer)
 
-	#if label == "LABEL_1":
-	#	outcome = "Urgent Message"
-	#else:
-	#	outcome = "Non-urgent Message"
-	
-	res = render_template('result.html', prediction=label, message=message)
-	
-	return res
+    output = classifier(message)
+    label = output[0]["label"]
+
+    res = render_template('result.html', prediction=label, 
+                        message=message, model=selected_model)
+    return res
 
 
 if __name__ == '__main__':
