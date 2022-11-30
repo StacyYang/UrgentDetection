35a0d47b5b3666498b13b6a54b74d741a5ccc3a9
Andy Le <andy.quile@gmail.com>
2022-11-29 19:34:56 -0800

    Add method to extract features of audio file; create methods to process features and display result

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
+import librosa
+import numpy as np
+import os 
 import pandas as pd 
+import pickle
 import torch
 
 from transformers import BertConfig, BertTokenizer, BertForSequenceClassification
@@ -6,11 +10,17 @@ from transformers import DistilBertConfig, DistilBertTokenizer, DistilBertForSeq
 from transformers import ErnieConfig, ErnieForSequenceClassification
 from transformers import pipeline
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for, abort
+from werkzeug.utils import secure_filename
 
+from sklearn.linear_model import LogisticRegression
 
 app = Flask(__name__)
 
+app.config['UPLOAD_EXTENSIONS'] = ['.wav']
+app.config['UPLOAD_PATH'] = 'uploads'
+
+model_log = pickle.load(open("./saved_models/logistic_regression.pkl", "rb"))
 # Bert 
 bert_tokenizer = BertTokenizer.from_pretrained("./bert_base_model")
 bert_model = BertForSequenceClassification.from_pretrained("./bert_base_model")
@@ -24,9 +34,62 @@ dbert2_model = DistilBertForSequenceClassification.from_pretrained("./reviews_se
 ernie_tokenizer = BertTokenizer.from_pretrained("./ernie_2_base_model")
 ernie_model = ErnieForSequenceClassification.from_pretrained("./ernie_2_base_model")
 
+def extract_features(signal, sr):
+    # Compute root-mean-square (RMS) value for each frame, either from the audio samples y or from a spectrogram S.
+    rms = librosa.feature.rms(signal + 0.0001)[0]
+    rms_mean = np.mean(rms)
+    rms_std = np.std(rms)
+
+    # mel frequnecy cepstral coefficients
+    # mfcc = librosa.feature.mfcc(y=signal, n_mfcc=13, sr=sr)
+    mfccs = np.mean(librosa.feature.mfcc(y=signal, sr=sr, n_mfcc=13).T, axis=0)
+    delta_mfccs = librosa.feature.delta(mfccs)
+    delta2_mfccs = librosa.feature.delta(mfccs, order=2)
+    
+    # The STFT represents a signal in the time-frequency domain by computing discrete Fourier transforms (DFT) over short overlapping windows.
+    stft = np.abs(librosa.stft(signal))
+
+    pitches, magnitudes = librosa.piptrack(signal, sr=sr, S=stft, fmin=70, fmax=400)
+    pitch = []
+    for i in range(magnitudes.shape[1]):
+        index = magnitudes[:, 1].argmax()
+        pitch.append(pitches[index, i])
+    # print('pitch: ', pitch)
+    pitch_tuning_offset = librosa.pitch_tuning(pitches)
+    pitch_mean = np.mean(pitch)
+    pitch_std = np.std(pitch)
+
+    flatness = np.mean(librosa.feature.spectral_flatness(y=signal))
+
+    cent = librosa.feature.spectral_centroid(y=signal, sr=sr)
+    centroid = cent / np.sum(cent)
+    centroid_mean = np.mean(centroid)
+    centroid_std = np.std(centroid)
+
+    ext_features = np.array([
+        rms_mean, rms_std, pitch_tuning_offset, pitch_mean, pitch_std,
+        flatness, centroid_mean, centroid_std])
+
+    extract_features = np.concatenate((ext_features, mfccs, delta_mfccs, delta2_mfccs))
+
+    return extract_features
+
+
 @app.route('/')
 def home():
     return render_template('home.html')
+
+# Accepting File Submissions & Securing file uploads
+@app.route('/', methods=['POST'])
+def upload_files():
+    uploaded_file = request.files['file']
+    filename = secure_filename(uploaded_file.filename)
+    if filename != '':
+        file_ext = os.path.splitext(filename)[1]
+        if file_ext not in app.config['UPLOAD_EXTENSIONS']:
+            abort(400)
+        uploaded_file.save(os.path.join(app.config['UPLOAD_PATH'], filename))
+    return redirect(url_for('home'))
 
 @app.route('/predict', methods=['POST'])
 def predict():
@@ -58,12 +121,21 @@ def predict():
                         message=message, model=selected_model)
     return res
 
-@app.route('/reveal', methods=['POST'])
+@app.route('/reveal', methods=['GET', 'POST'])
 def reveal():
-    afile = request.form['audiofile']
+    columns = ['rms_mean', 'rms_std', 'pitch_tuning_offset', 'pitch_mean', 'pitch_std', 'flatness', 'centroid_mean', 'centroid_std']
+    columns_plus = columns + [(lambda x: "mfcc_" + str(x))(x) for x in range(1, 14)] + [(lambda x: "delta_mfccs_" + str(x))(x) for x in range(1, 14)] + [(lambda x: "delta2_mfccs_" + str(x))(x) for x in range(1, 14)]
+    df= pd.DataFrame(columns = columns_plus)
+
+    audio_file = request.files['file']
+    signal, sr = librosa.load(audio_file)
+    features_all = list(extract_features(signal, sr))
+    df = df.append(pd.DataFrame(features_all, index=columns_plus).transpose(), ignore_index=True)
+
+    speech_prediction = model_log.predict(df)[0]
     
     return render_template('emotion_display.html',
-                        audiofile=afile)
+                        audiofile=audio_file, prediction=speech_prediction)
 
 if __name__ == '__main__':
 	app.run(debug=True)
